296483b93019707e7ffbd2d578f2e448952999fb
anshuraghv12 <134767699+anshuraghv12@users.noreply.github.com>
2025-02-26 14:24:24 +0530

    update code

--- src/data/profileData.ts
+++ src/data/profileData.ts
@@ -111,13 +111,13 @@ export const profileData: Profile[] = [
   {
     name: "Vaibhav Sarkar",
     profilePicture: "https://media.licdn.com/dms/image/v2/D5635AQFarJixbGWYIA/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1731416197695?e=1741075200&v=beta&t=0TWpRJwET2aiICaqSPvBo6JCCgkiH0HDzWc3meJKBTg",
-    company: "Jagannath University",
+    company: "Ai Enthusiast",
     category: "data-specialist"
   },
   {
     name: "Darshit Srivastava",
     profilePicture: "https://media.licdn.com/dms/image/v2/C4E03AQGNEiIK_Hdp1A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1663585351751?e=1746057600&v=beta&t=EIU_wWMMO45ODd_toGDsql3OPh9fSVVPUsU0HOpKDKM",
-    company: "ABES Engineering College",
+    company: "Ai Enthusiast",
     category: "data-specialist"
   },
   {
@@ -150,12 +150,24 @@ export const profileData: Profile[] = [
     company: "Girikon Solutions ",
     category: "data-specialist"
   },
+  {
+    name: "Ishank Agarwal",
+    profilePicture: "https://media.licdn.com/dms/image/v2/C4E03AQGIuD9OsJVPZA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1517469314609?e=1746057600&v=beta&t=LWze2-KybTgq5T-CnpTKxSc7UkiGV1GOHqn7TjXbUYY",
+    company: "Upwork ",
+    category: "data-specialist"
+  },
+  {
+    name: "Shreyash Baghel",
+    profilePicture: "https://media.licdn.com/dms/image/v2/D5635AQG2dwQCgxRyhw/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1734427946133?e=1741161600&v=beta&t=IryJrE1WLninGA_BGuOe3s-m2k9rvV-eUA2CtWBFVCg",
+    company: "Quantiphi ",
+    category: "data-specialist"
+  },
   
   // AI Tools Expert
   {
     name: "Amish Agarwal",
     profilePicture: "https://media.licdn.com/dms/image/v2/D5635AQENsoHoFIbJSw/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1710400296916?e=1741075200&v=beta&t=__n7bDMCfnfho2H4rD7meLq4bBU-V_3NZIJEteAXkvs",
-    company: "Jodhpur Institute ",
+    company: "Ai Enthusiast ",
     category: "ai-tool-expert"
   },
   {
@@ -171,39 +183,44 @@ export const profileData: Profile[] = [
 },{
   name: "Harvinder Singh",
   profilePicture: "https://media.licdn.com/dms/image/v2/D5635AQE9LIF5v8rRJw/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1735738339169?e=1741075200&v=beta&t=uKQOVESWSjg6wQmjAr3p1bECmBYNfOB2wrv4yYpkYPk",
-  company: "Self",
+  company: "Ai Enthusiast",
   category: "ai-tool-expert"
 },
   // AI Model Trainer
   {
     name: "Bhavya Sharma",
     profilePicture: "https://media.licdn.com/dms/image/v2/D4D03AQHD6G7OIaAWOA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1701109331010?e=1746057600&v=beta&t=gWgaoRxAY9ACqU8fRGb8oL-CaesSLNDzN5QWIh4hmpI",
-    company: "Mahaveer College",
+    company: "Ai Enthusiast",
     category: "ai-model-trainer"
   },
   
 {
   name: "Sahil Rakhaiya",
   profilePicture: "https://media.licdn.com/dms/image/v2/D4D03AQE9ijmVIF2vLg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1730110647432?e=1746057600&v=beta&t=rbg2aZ7gHhWn80G8SD0SMWuP-Iz92-fp0QvkZMlhMXc",
-  company: "Marwadi University",
+  company: "Ai Enthusiast",
   category: "ai-model-trainer"
 },
 
 {
   name: "Jeeya Agrawal",
   profilePicture: "https://media.licdn.com/dms/image/v2/D5603AQEKbwztA4IgzQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1731760078957?e=1746057600&v=beta&t=DkQf3ZuIlEACXZv5RX_Icgwm7y3r7o9fsCDcc4xveAU",
-  company: "Vision Institute of Technology",
+  company: "Ai Enthusiast",
   category: "ai-model-trainer"
 },
 
 {
   name: "Soumen Bhunia",
   profilePicture: "https://media.licdn.com/dms/image/v2/D5603AQEdlf4PBpP9Mg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1723874969891?e=1746057600&v=beta&t=gJZhhzAxzpcMpfcmkuLB8IpOfV-gUEgQ1Xv0L8RzAto",
-  company: "BRAINWARE UNIVERSITY",
+  company: "Ai Enthusiast",
   category: "ai-model-trainer"
 },
 
-
+{
+  name: "Tanmay agarwal",
+  profilePicture: "https://media.licdn.com/dms/image/v2/D5603AQFXTTr4YmNrSw/profile-displayphoto-shrink_200_200/B56ZRmSL9AGsAY-/0/1736882831613?e=2147483647&v=beta&t=HoK3UBeDMCUq303npzjfDVqzcKzq1Ff1Oi0ft2_R1Tk",
+  company: "Okta",
+  category: "ai-model-trainer"
+},
 
 // data center expert
 {
@@ -226,13 +243,27 @@ export const profileData: Profile[] = [
   company: "Mahindra",
   category: "data-center-expert"
 },
+{
+  name: "Anand Kumar",
+  profilePicture: "https://media.licdn.com/dms/image/v2/D5635AQHFS-YzemPF4g/profile-framedphoto-shrink_100_100/profile-framedphoto-shrink_100_100/0/1715078135641?e=1741165200&v=beta&t=T5Rh4Kr2S3QNaCwCa3c5ZCtcytsSongcNMl70_iJOO8",
+  company: "Infinite computer solutions ",
+  category: "data-center-expert"
+},
+{
+  name: "Rohit khapre",
+  profilePicture: "https://media.licdn.com/dms/image/v2/D4D03AQFZOkBTNajpoQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1698177448028?e=1746057600&v=beta&t=w-me26LE06G_cKVPnRsKr0vN7coscQQiRoA2mp2sGW0",
+  company: "Atrina technologies ",
+  category: "data-center-expert"
+},
+
+
 
 // networking specialist 
 
 {
   name: "Atharv Bharadwaj",
   profilePicture: "", // No LinkedIn profile picture available in Column 9
-  company: "GLA UNIVERSITY",
+  company: "Ai Enthusiast",
   category: "networking-specialist"
 },
 
@@ -249,14 +280,14 @@ export const profileData: Profile[] = [
 {
   name: "Dinesh Ajmera",
   profilePicture: "https://media.licdn.com/dms/image/v2/D5635AQFfOMSNzy6Vaw/profile-framedphoto-shrink_400_400/profile-framedphoto-shrink_400_400/0/1723474421068?e=1741075200&v=beta&t=08OHGBPauP3-jYvleSQjoVrIM0gmYK_8QzxdjJXF7lo",
-  company: "Arya Institute of Engineering",
+  company: "Ai Enthusiast",
   category: "cooling-system-innovator"
 },
   // Social Media Manager
   {
     name: "Sayantan Samanta",
     profilePicture: "https://media.licdn.com/dms/image/v2/D4D03AQFMUB-gK3bhXw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1680722905923?e=1746057600&v=beta&t=LxiWZXlZ0aG1x3JLzwNa-Zz51ZZZJ130Dv3DQeRbmBw",
-    company: "GCETT-Berhampore",
+    company: "Ai Enthusiast",
     category: "social-media-manager"
   },
 
@@ -264,14 +295,14 @@ export const profileData: Profile[] = [
   {
     name: "Aravind Reddy",
     profilePicture: "https://media.licdn.com/dms/image/v2/D5603AQGxzLHts8o1Rw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724022449925?e=1746057600&v=beta&t=GVwZRYqhQ8AwO7DZnPGI87qafWnB1nXPHMjiMt6O3MA",
-    company: "CGI",
+    company: "Ai Enthusiast",
     category: "data-scrapping-team"
 },
 
 {
     name: "Sharan Hiremani",
     profilePicture: "/assets/Collabrote/Sharan Hiremani.jpg", // Base64 placeholder image
-    company: "KLS GIT",
+    company: "Ai Enthusiast",
     category: "data-scrapping-team"
 },
 
@@ -285,8 +316,32 @@ export const profileData: Profile[] = [
 {
     name: "Nikhil",
     profilePicture: "https://media.licdn.com/dms/image/v2/D5603AQF2hTQeH6YoIw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1731132380817?e=1746057600&v=beta&t=hHMISOilo59ZLHhaUPbffB7LLhila3TU21AgMQQP6rw",
-    company: "TITS",
+    company: "Ai Enthusiast",
     category: "ai-tool-expert"
+},
+{
+  name: "Puru Sharma",
+  profilePicture: "https://media.licdn.com/dms/image/v2/D5603AQGlge4hPijydg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1709662571114?e=1746057600&v=beta&t=e4oyvOyc_2nesNWoZctj1eNjzExVrUGfcFDOxnUxczI",
+  company: "Edurigo Technologies",
+  category: "ai-tool-expert"
+},
+{
+  name: "Tanmay Agarwal",
+  profilePicture: "https://media.licdn.com/dms/image/v2/D5603AQFXTTr4YmNrSw/profile-displayphoto-shrink_400_400/B56ZRmSL9AGsAg-/0/1736882831629?e=1746057600&v=beta&t=1Rk-xuR1vZENb3m8_fhnvp_UGgOGByfPiDaP7qG39vA",
+  company: "Okta",
+  category: "ai-tool-expert"
+},
+{
+  name: "Rahul Bharuka,",
+  profilePicture: "https://media.licdn.com/dms/image/v2/D5603AQHsco_gb7JIDw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1696185408620?e=1746057600&v=beta&t=biNEo2r3z5XCR54wm4xa36dpLl0uEyTZT6xiv6JQIas",
+  company: "Dentrils ",
+  category: "ai-tool-expert"
+},
+{
+  name: "Puru Sharma",
+  profilePicture: "https://media.licdn.com/dms/image/v2/D5603AQGlge4hPijydg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1709662571114?e=1746057600&v=beta&t=e4oyvOyc_2nesNWoZctj1eNjzExVrUGfcFDOxnUxczI",
+  company: "Edurigo Technologies",
+  category: "ai-tool-expert"
 }
 ];
 // Export the categorized data directly
