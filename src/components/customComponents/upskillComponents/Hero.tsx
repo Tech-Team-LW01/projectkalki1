f45da41e252de670def7613c5275ffa04600cc02
Sumit <tonystark83033@gmail.com>
2025-03-20 11:41:11 +0530

    change hero section

--- src/components/customComponents/upskillComponents/Hero.tsx
+++ src/components/customComponents/upskillComponents/Hero.tsx
@@ -15,23 +15,24 @@ const Hero = () => {
             <h1 className="text-3xl md:text-5xl font-bold leading-tight">
   
 
-              
-            Empower{" "}
+            Free {" "}
               <span className="text-[#000080] relative">
-              India's AI
+              AI Foundation
               
 <img src="/assets/CurlLine.svg" alt="curve line" className="absolute w-full bottom-[-2px] left-0    transform scale-x-105" />
               </span>{" "}
-              Future
+              Training
             </h1>
             <p className="text-lg text-gray-600">
-            Join our Free AI Training to support the IndiaAI Mission and boost national upskilling efforts.
+       
+
+          Empowering India's AI Future: Join our Free AI Training to support the IndiaAI Mission and boost national upskilling efforts.
 
             </p>
             <div className="flex flex-col sm:flex-row gap-4">
             {/* https://forms.gle/qwzu4w367nP59mgU6 */}
             <a href="https://forms.gle/qwzu4w367nP59mgU6" target="blank">
-              <Button className="bg-gradient-to-r from-[#046a38] to-[#ff6820] hover:opacity-90 transition-opacity text-white">Join Now</Button></a>
+              <Button className="bg-gradient-to-r from-[#046a38] to-[#ff6820] hover:opacity-90 transition-opacity text-white">Join for Free</Button></a>
               <a href="https://www.linkedin.com/in/vimaldaga/" target="blank">
               <Button variant="outline" className="hover:bg-gradient-to-r from-[#046a38] to-[#ff6820] hover:text-white" >
  By Mr Vimal Daga
